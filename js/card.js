12cb5892efd29568ba860d3c28cff9aefd6cfa29
P-Hemal <abdulhakim.hemal.99@gmail.com>
2024-04-04 14:06:58 +0600

    task 1 complete

--- js/card.js
+++ js/card.js
@@ -2,14 +2,15 @@ document.getElementById('btn-discount').addEventListener('click', function () {
     const regularPriceField = document.getElementById('regular-price');
     const regularPriceString = regularPriceField.innerText;
     const regularPrice = parseFloat(regularPriceString);
-
+    
     const discountField = document.getElementById('discount-field');
     const discountPriceString = discountField.innerText;
     const discountPrice = parseFloat(discountPriceString);
     
-    const afterDiscountPrice = (regularPrice / 100) * 70;
     
-    discountPrice.innerText = afterDiscountPrice;
+    const afterDiscountPriceString = (regularPrice / 100) * 70;
+    const afterDiscountPrice = parseFloat(afterDiscountPriceString)
+    discountField.innerText = afterDiscountPrice;
 
 
 })
