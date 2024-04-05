252105cd76e349ac2051a1ec1ef96ecd8c4f836c
P-Hemal <abdulhakim.hemal.99@gmail.com>
2024-04-05 12:03:16 +0600

    discount

---
+++ js/card-2.js
@@ -0,0 +1,21 @@
+document.getElementById('btn-apply').addEventListener('click', function(){
+    const couponField = document.getElementById('text-coupon');
+    const couponCode = couponField.Value;
+    const discountField = document.getElementById('discount-field')
+    const discountAmountString = discountField.innerText;
+    const discountAmount = parseFloat.discountAmountString;
+    const afterDiscountPriceString = (regularPrice / 100) * 70;
+    if(couponCode === 'DISC30'.toUpperCase){
+        
+    }
+})
+
+
+
+
+
+
+
+document.getElementById('text-coupon'),addEventListener('keyup', function(event){
+    const text = 
+})
