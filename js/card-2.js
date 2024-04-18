ae2d41842dcc78987e6c5270617c496c3ccca98f
P-Hemal <abdulhakim.hemal.99@gmail.com>
2024-04-18 21:58:56 +0600

    updated

---
+++ js/card-2.js
@@ -0,0 +1,5 @@
+document.getElementById('btn-apply').addEventListener('click', function(){
+    const regularPriceField = document.getElementById(regular-price)
+    const regularPriceString = regularPriceField.innerText;
+    const regularPrice = parseFloat(regularPriceString);
+})
